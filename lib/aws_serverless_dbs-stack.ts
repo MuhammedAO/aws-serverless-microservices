67519af876f0ff8d2e5c472868c3982c8ed26d8a
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-09-30 01:40:09 +0100

    product dynamo db table and lambda fn

--- lib/aws_serverless_dbs-stack.ts
+++ lib/aws_serverless_dbs-stack.ts
@@ -1,16 +1,45 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
+
+
 
 export class AwsServerlessDbsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, 'product', {
+     partitionKey: {name: 'id', type: AttributeType.STRING},
+     tableName: 'product',
+     removalPolicy: RemovalPolicy.DESTROY,
+     billingMode: BillingMode.PAY_PER_REQUEST
+    })
+    //external-modules: to be present when running this fn
+    const nodeJsFnProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk'
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_14_X
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsServerlessDbsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   //product microservice lambda function
+   const productFunction = new NodejsFunction(this, 'product', {
+    entry: join(__dirname, `/../src/product/index.js`),
+    ...nodeJsFnProps
+   })
+
+   productTable.grantReadWriteData(productFunction)
+
   }
 }
