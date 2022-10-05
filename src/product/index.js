743dae3c219b53584342e8c7994e4ce4fd1ab619
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-10-05 15:54:31 +0100

    key -> Key

--- src/product/index.js
+++ src/product/index.js
@@ -5,55 +5,73 @@ import {
   PutItemCommand,
   DeleteItemCommand,
   UpdateItemCommand,
-  QueryCommand
+  QueryCommand,
 } from "@aws-sdk/client-dynamodb"
 import { dynamoDbClient } from "./dbClient"
-import { uuid as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from "uuid"
 
 exports.handler = async function (event) {
   console.log("request", JSON.stringify(event, null, 2))
+  console.log('pathParams', event.pathParameters)
   let body
-  switch (event.httpMethod) {
-    case "GET":
-      if(event.queryStringParameters !== null) {
-        body = await getProductsByCategory(event)
-      } 
-      else if (event.pathParameters !== null) {
-        body = await getProduct(event.pathParameters.id)
-      } else {
-        body = await getAllProducts()
-      }
-    case "POST":
-      body = await createProduct(event)
-      break
-    case "DELETE":
-      body = await deleteProduct(event.pathParameters.id)
-      break
-    case "PUT":
-      body = await updateProduct(event)
-      break
 
-    default:
-      throw new Error(`Unsupported route: "${event.httpMethod}"`)
-  }
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: JSON.stringify({
-      message: `Successfully finished operation: "${event.httpMethod}"`,
-      body: body,
-    }),
+  try {
+    switch (event.httpMethod) {
+      case "GET":
+        if (event.queryStringParameters != null) {
+          body = await getProductsByCategory(event)
+          event.pathParameters
+        } else if (event.pathParameters != null) {
+          body = await getProduct(event)
+        } else {
+          body = await getAllProducts()
+        }
+        break
+      case "POST":
+        body = await createProduct(event)
+        break
+      case "DELETE":
+        body = await deleteProduct(event)
+        break
+      case "PUT":
+        body = await updateProduct(event)
+        break
+
+      default:
+        throw new Error(`Unsupported route: "${event.httpMethod}"`)
+    }
+    console.log("body", body)
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify({
+        message: `Successfully finished operation: "${event.httpMethod}"`,
+        body: body,
+      }),
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `failed to perform operation`,
+        errorMsg: error.stack,
+        errorStack: error.stack,
+      }),
+    }
   }
 }
 
-const getProduct = async (productId) => {
-  console.log("getProduct")
+const getProduct = async (event) => {
+  console.log("getProduct event object", event, `\n`, event.pathParameters.id )
 
   try {
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      key: marshall({ id: productId }),
+      Key: marshall({ id: event.pathParameters.id }),
     }
+
+    console.log('params from getProduct', params)
 
     const { Item } = await dynamoDbClient.send(new GetItemCommand(params))
 
@@ -107,12 +125,12 @@ const createProduct = async (event) => {
   }
 }
 
-const deleteProduct = async (productId) => {
-  console.log("Delete in progress", productId)
+const deleteProduct = async (event) => {
+  // console.log("Delete in progress", productId)
   try {
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      key: marshall({ id: productId }),
+      Key: marshall({ id: event.pathParameters.id }),
     }
 
     const deleteResult = await dynamoDbClient.send(
