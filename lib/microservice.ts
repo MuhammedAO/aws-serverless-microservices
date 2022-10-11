649c0d2526c77de9360d5d0dfaf74fbab646fa8f
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-10-11 16:40:50 +0100

    refactor

--- lib/microservice.ts
+++ lib/microservice.ts
@@ -9,21 +9,30 @@ import { join } from "path"
 
 interface XyzMicroservicesProps {
   productTable: ITable
+  basketTable: ITable
 }
 export class XyzMicroservices extends Construct {
   public readonly productMicroservice: NodejsFunction
+  public readonly basketMicroservice: NodejsFunction
 
   constructor(scope: Construct, id: string, props: XyzMicroservicesProps) {
     super(scope, id)
 
+    this.productMicroservice = this.createProductLambdaFunction(
+      props.productTable
+    )
+    this.basketMicroservice = this.createBasketLambdaFunction(props.basketTable)
+  }
+
+  private createProductLambdaFunction(productTable: ITable): NodejsFunction {
     //external-modules: to be present when running this fn
-    const nodeJsFnProps: NodejsFunctionProps = {
+    const productFnProps: NodejsFunctionProps = {
       bundling: {
         externalModules: ["aws-sdk"],
       },
       environment: {
         PRIMARY_KEY: "id",
-        DYNAMODB_TABLE_NAME: props.productTable.tableName,
+        DYNAMODB_TABLE_NAME: productTable.tableName,
       },
       runtime: Runtime.NODEJS_14_X,
     }
@@ -31,11 +40,34 @@ export class XyzMicroservices extends Construct {
     //product microservice lambda function
     const productFunction = new NodejsFunction(this, "product", {
       entry: join(__dirname, `/../src/product/index.js`),
-      ...nodeJsFnProps,
+      ...productFnProps,
     })
     //grant lambda fn permissions to mutate dynamoDB
-    props.productTable.grantReadWriteData(productFunction)
+    productTable.grantReadWriteData(productFunction)
 
-    this.productMicroservice = productFunction
+    return productFunction
+  }
+
+  private createBasketLambdaFunction(basketTable: ITable): NodejsFunction {
+    const basketFnProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      environment: {
+        PRIMARY_KEY: "userName",
+        DYNAMODB_TABLE_NAME: basketTable.tableName,
+      },
+      runtime: Runtime.NODEJS_14_X,
+    }
+
+    //basket microservice lambda function
+    const basketFunction = new NodejsFunction(this, "basket", {
+      entry: join(__dirname, `/../src/basket/index.js`),
+      ...basketFnProps,
+    })
+    //grant lambda fn permissions to mutate dynamoDB
+    basketTable.grantReadWriteData(basketFunction)
+
+    return basketFunction
   }
 }
