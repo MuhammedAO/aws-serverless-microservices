0d23d6ab5e124b044a1e6265767d7665a5dbc919
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-10-11 13:32:18 +0100

    use constructs

--- lib/aws_serverless_dbs-stack.ts
+++ lib/aws_serverless_dbs-stack.ts
@@ -1,65 +1,23 @@
 import * as cdk from "aws-cdk-lib"
-import { RemovalPolicy } from "aws-cdk-lib"
-import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway"
-import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb"
-import { Runtime } from "aws-cdk-lib/aws-lambda"
-import {
-  NodejsFunction,
-  NodejsFunctionProps,
-} from "aws-cdk-lib/aws-lambda-nodejs"
 
 import { Construct } from "constructs"
-import { join } from "path"
+
+import { XyzApiGateway } from "./apigateway"
+import { XyzDatabse } from "./database"
+import { XyzMicroservices } from "./microservice"
 
 export class AwsServerlessDbsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const productTable = new Table(this, "product", {
-      partitionKey: { name: "id", type: AttributeType.STRING },
-      tableName: "product",
-      removalPolicy: RemovalPolicy.DESTROY,
-      billingMode: BillingMode.PAY_PER_REQUEST,
-    })
-    //external-modules: to be present when running this fn
-    const nodeJsFnProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: ["aws-sdk"],
-      },
-      environment: {
-        PRIMARY_KEY: "id",
-        DYNAMODB_TABLE_NAME: productTable.tableName,
-      },
-      runtime: Runtime.NODEJS_14_X,
-    }
+    const database = new XyzDatabse(this, "database")
 
-    //product microservice lambda function
-    const productFunction = new NodejsFunction(this, "product", {
-      entry: join(__dirname, `/../src/product/index.js`),
-      ...nodeJsFnProps,
+    const microservices = new XyzMicroservices(this, "microsrvs", {
+      productTable: database.productTable,
     })
 
-    //grant lambda fn permissions to mutate dynamoDB
-    productTable.grantReadWriteData(productFunction)
-
-    //Product microservice api-gateway
-    //root name = product
-
-    const apiGW = new LambdaRestApi(this, "productApi", {
-      restApiName: "product service",
-      handler: productFunction,
-      proxy: false,
+    const apigateway = new XyzApiGateway(this, "Apigateway", {
+      productMicroservice: microservices.productMicroservice,
     })
-
-    const product = apiGW.root.addResource("product")
-    product.addMethod("GET") //GET /product
-    product.addMethod("POST") //POST /product
-
-    const singleProduct = product.addResource("{id}") //product/{id}
-    singleProduct.addMethod("GET") //GET /product/{id}
-    singleProduct.addMethod("PUT") //PUT /product/{id}
-    singleProduct.addMethod("DELETE") //DELETE /product/{id}
-
-    
   }
 }
