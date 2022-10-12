be35f2921fdc706620bc58bb5caa4e7d405b72cf
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-10-12 15:50:48 +0100

    create basket api

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -5,22 +5,27 @@ import { Construct } from "constructs"
 
 interface XyzApiGatewayProps {
   productMicroservice: IFunction
+  basketMicroservice: IFunction
 }
 
 export class XyzApiGateway extends Construct {
   constructor(scope: Construct, id: string, props: XyzApiGatewayProps) {
     super(scope, id)
 
+    this.createProductApi(props.productMicroservice)
+    this.createBasketApi(props.basketMicroservice)
+  }
+
+  private createProductApi(productMicroservice: IFunction) {
     //Product microservice api-gateway
     //root name = product
-
-    const apiGW = new LambdaRestApi(this, "productApi", {
+    const productApiGw = new LambdaRestApi(this, "productApi", {
       restApiName: "product service",
-      handler: props.productMicroservice,
+      handler: productMicroservice,
       proxy: false,
     })
 
-    const product = apiGW.root.addResource("product")
+    const product = productApiGw.root.addResource("product")
     product.addMethod("GET") //GET /product
     product.addMethod("POST") //POST /product
 
@@ -28,5 +33,22 @@ export class XyzApiGateway extends Construct {
     singleProduct.addMethod("GET") //GET /product/{id}
     singleProduct.addMethod("PUT") //PUT /product/{id}
     singleProduct.addMethod("DELETE") //DELETE /product/{id}
+  }
+
+  private createBasketApi(basketMicroservice: IFunction) {
+    // Basket microservices api gateway
+    // root name = basket
+    const basketApiGw = new LambdaRestApi(this, "basketApi", {
+      restApiName: "basket service",
+      handler: basketMicroservice,
+      proxy: false,
+    })
+    const basket = basketApiGw.root.addResource("basket")
+    basket.addMethod("GET") // GET /basket
+    basket.addMethod("POST") // POST /basket
+
+    const singleBasket = basket.addResource("{userName}")
+    singleBasket.addMethod("GET") // GET /basket/{userName}
+    singleBasket.addMethod("DELETE") // DELETE /basket/{userName}
   }
 }
