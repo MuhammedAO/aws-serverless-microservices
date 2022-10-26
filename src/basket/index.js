db6086af646a801b9a069679522733285c0a350b
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-10-26 13:58:44 +0100

    prepareOrderPayload

--- src/basket/index.js
+++ src/basket/index.js
@@ -67,7 +67,7 @@ const getBasket = async (userName) => {
     console.log("item", Item)
     return Item ? unmarshall(Item) : {}
   } catch (error) {
-    console.log('getBasketError',error)
+    console.log("getBasketError", error)
     throw new Error(error)
   }
 }
@@ -84,7 +84,7 @@ const getAllBaskets = async () => {
 
     return Items ? Items.map((item) => unmarshall(item)) : {}
   } catch (error) {
-    console.log('getAllBaskets error',error)
+    console.log("getAllBaskets error", error)
     throw new Error(error)
   }
 }
@@ -108,7 +108,7 @@ const createBasket = async (event) => {
 
     return result
   } catch (error) {
-    console.log('createBasketError',error)
+    console.log("createBasketError", error)
     throw new Error(error)
   }
 }
@@ -130,4 +130,47 @@ const deleteBasket = async (userName) => {
     throw new Error(error)
   }
 }
-const checkoutBasket = async () => {}
+const checkoutBasket = async (event) => {
+  const checkoutRequest = JSON.parse(event.body)
+
+  if (checkoutRequest === null || checkoutRequest.userName === null) {
+    throw new Error(`Username is required but not found: ${checkoutRequest}`)
+  }
+
+  // 1- Get existing basket with items
+  const basket = await getBasket(checkoutRequest.userName)
+  // 2- create an event json object with basket items,
+  const checkoutPayload = prepareOrderPayload(checkoutRequest, basket)
+  // calculate totalprice, prepare order create json data to send ordering microservice
+
+  // 3- publish an event to eventbridge - this will subscribe by order microservice and start ordering process.
+  await publishCheckoutBasketEvent(checkoutPayload)
+  //4 - clear basket
+  await deleteBasket(checkoutRequest.userName)
+}
+
+const prepareOrderPayload = (checkoutRequest, basket) => {
+  console.log("prepareOrderPayload")
+
+  // prepare order payload -> calculate totalprice and combine checkoutRequest and basket items
+  // aggregate and enrich request and basket data in order to create order payload
+  try {
+    if (basket == null || basket.items == null) {
+      throw new Error(`basket should exist in items: "${basket}"`)
+    }
+
+    // calculate totalPrice
+    let totalPrice = 0
+    basket.items.forEach((item) => (totalPrice = totalPrice + item.price))
+    checkoutRequest.totalPrice = totalPrice
+    console.log(checkoutRequest)
+
+    // copies all properties from basket into checkoutRequest
+    Object.assign(checkoutRequest, basket)
+    console.log("Success prepareOrderPayload, orderPayload:", checkoutRequest)
+    return checkoutRequest
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
