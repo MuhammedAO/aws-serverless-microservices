6de61ef7bba8661d32ba90202b7d8cec95a91719
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-10-27 17:54:06 +0100

    event bridge invocation

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -1,8 +1,26 @@
+const { PutItemCommand } = require("@aws-sdk/client-dynamodb")
+const { marshall } = require("@aws-sdk/util-dynamodb")
+const { dynamoDbClient } = require("./dbClient")
 
-exports.handler = async function(event) {
-   return {
-      statusCode: 200,
-      body: 'Hello World!'
-  };
+exports.handler = async function (event) {
+  if (event["detail-type"] !== undefined) {
+    // EventBridge Invocation
+    await eventBridgeInvocation(event)
+  } else {
+    // API Gateway Invocation -- return sync response
+   //  return await apiGatewayInvocation(event)
+  }
 
-};
+  return {
+    statusCode: 200,
+    body: "Hello World!",
+  }
+}
+
+const eventBridgeInvocation = async (event) => {
+  console.log(`eventBridgeInvocation function. event : "${event}"`)
+
+  // create order item into db
+  await createOrder(event.detail)
+}
+
